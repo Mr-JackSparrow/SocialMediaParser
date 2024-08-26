989992aa8a9d116253000ec207e71bb36c3b0efe
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2024-08-26 15:55:30 +0530

    added singleton driver, created autoChat.

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         AutoChat autoChat = new AutoChat();
         autoChat.startAutoChat();
+        System.out.println("printit");
     }
 }
