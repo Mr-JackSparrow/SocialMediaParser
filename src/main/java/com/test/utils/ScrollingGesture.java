fc1fd8b751caeffc1865403fa0a250a84d9cdd05
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2024-08-28 14:59:23 +0530

    created ScrollingGesture, added swipeUp.

---
+++ src/main/java/com/test/utils/ScrollingGesture.java
@@ -0,0 +1,21 @@
+package com.test.utils;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Collections;
+
+public class ScrollingGesture {
+
+    public static void scrollLeft(){}
+
+    public static void scrollRight(){}
+
+    public static void scrollUp(AppiumDriver driver){}
+
+    public static void scrollDown(){}
+}
