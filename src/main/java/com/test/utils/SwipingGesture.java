13b2ce474eaaf5ee887e271a0467b8c966a8ba3d
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2024-08-28 23:24:09 +0530

    completed SwipingGesture by adding swipeRight, swipeLeft, swipeUp, SwipeDown.

--- src/main/java/com/test/utils/SwipingGesture.java
+++ src/main/java/com/test/utils/SwipingGesture.java
@@ -14,11 +14,64 @@ import java.util.Map;
 
 public class SwipingGesture {
 
-    public static void swipeLeft(){}
+    public static void swipeLeft(AppiumDriver driver){
 
-    public static void swipeRight(AppiumDriver driver){}
+        Dimension dimension = driver.manage().window().getSize();
+
+        int startX = (int) (dimension.getWidth() * 0.6);
+        int endX = (int) (dimension.getWidth() * 0.3);
+
+        int y = dimension.getHeight() / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+        Sequence sequence = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, y))
+                .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(finger.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endX, y))
+                .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(sequence));
+    }
+
+    public static void swipeRight(AppiumDriver driver){
+
+        Dimension dimension = driver.manage().window().getSize();
+
+        int startX = (int) (dimension.getWidth() * 0.3);
+        int endX = (int) (dimension.getWidth() * 0.6);
+
+        int y = dimension.getHeight() / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+        Sequence sequence = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, y))
+                .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(finger.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endX, y))
+                .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(sequence));
+    }
 
     public static void swipeDown(AppiumDriver driver){
+
+        Dimension dimension = driver.manage().window().getSize();
+
+        int x = dimension.getWidth() / 2;
+
+        int startY = (int) (dimension.getHeight() * 0.3);
+        int endY = (int) (dimension.getHeight() * 0.6);
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+        Sequence sequence = new Sequence(finger, 1)
+                .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), new Point(x, startY)))
+                .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(finger.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), new Point(x, endY)))
+                .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Collections.singletonList(sequence));
     }
 
     public static void swipeUp(AppiumDriver driver){
@@ -26,8 +79,8 @@ public class SwipingGesture {
         Dimension dimension = driver.manage().window().getSize();
 
         int x = dimension.getWidth() / 2;
-        int startY = (int) (dimension.getHeight() * 0.8);
-        int endY = (int) (dimension.getHeight() * 0.2);
+        int startY = (int) (dimension.getHeight() * 0.6);
+        int endY = (int) (dimension.getHeight() * 0.3);
 
         PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
